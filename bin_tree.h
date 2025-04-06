99ec95d6b8c2360d9e0099203946ad1330a51d75
NeIIor <egor.gabisov@gmail.com>
2025-04-06 20:47:09 +0300

    deletik

--- bin_tree.h
+++
@@ -1,8 +0,0 @@
-#ifndef __BIN_TREE_H__
-#define __BIN_TREE_H__
-
-#include "akinator.h"
-
-node_t* createNode (char* data, node_t* node);
-
-#endif // __BIN_TREE_H
--- html.h
+++
@@ -1,7 +0,0 @@
-#ifndef __HTML_H__
-#define __HTML_H__
-
-void __attribute__((constructor)) fillBeginningHtml ();
-void fillEndHtml ();
-
-#endif // __HTML_H__
